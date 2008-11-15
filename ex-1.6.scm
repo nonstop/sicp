8653043ae653e0e119ac3bc25f72b7da604b81ea
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2008-11-15 10:58:25 +0300

    comments in ex-1.6

--- ex-1.6.scm
+++ ex-1.6.scm
@@ -9,10 +9,16 @@
         (else else-clause))))
 
 (define (sqrt-iter guess x)
-;  (if (good-enough? guess x)
-  (new-if (good-enough? guess x)
+  (if (good-enough? guess x)
+; recursive call sqrt-iter -> new-if -> sqrt-iter -> new-if -> ...
+;  (new-if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x) x)))
+
+(define (sqrt-iter guess x)
+  (cond ((good-enough? guess x) guess)
+      (else (sqrt-iter (improve guess x) x))))
+
 
 (define (improve guess x)
   (average guess (/ x guess)))
