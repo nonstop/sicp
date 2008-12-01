dfd67d8f251d4125b8b0818d400bf4f9c5634248
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2008-12-01 21:06:06 +0300

    ex-1.11 closer

--- ex-1.11.scm
+++ ex-1.11.scm
@@ -1,11 +1,18 @@
 ; recursive process
 (define (f n)
-(cond ((< n 3) n)
-      (else (+ 
-              (f (- n 1)) 
-              (* 2 (f (- n 2))) 
-              (* 3 (f (- n 3))))
-            )))
+  (cond ((< n 3) n)
+        (else (+ 
+                (f (- n 1)) 
+                (* 2 (f (- n 2))) 
+                (* 3 (f (- n 3))))
+              )))
 
 ; iterative process
+(define (f-iter n)
+  (define (f-inner n i s2 s3)
+    (cond (> i (- n 2)) (+ i s2 s3)
+          (else (f-inner n (+ i 1) (* 2 s2) (* 3 s3))))
+    )
+  (f-inner n 0 0 0)
+  )
 
