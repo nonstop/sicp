894b1140ff2254d01ae4a2aa9092627bd850c49c
Evgeny <ratnikov.ev@gmail.com>
2008-12-02 10:59:05 +0300

    ex-1.11 finished

--- ex-1.11.scm
+++ ex-1.11.scm
@@ -1,18 +1,24 @@
+; utility function
+(define (f-sum a b c)
+  (+ a (* 2 b) (* 3 c)))
+
 ; recursive process
 (define (f n)
-  (cond ((< n 3) n)
-        (else (+ 
-                (f (- n 1)) 
-                (* 2 (f (- n 2))) 
-                (* 3 (f (- n 3))))
-              )))
+  (if (< n 3) n
+    (f-sum (f (- n 1)) (f (- n 2)) (f (- n 3)))))
 
 ; iterative process
 (define (f-iter n)
-  (define (f-inner n i s2 s3)
-    (cond (> i (- n 2)) (+ i s2 s3)
-          (else (f-inner n (+ i 1) (* 2 s2) (* 3 s3))))
+  (define (f-inner n i s1 s2 s3)
+    (display n) (display '" i=") (display i) (display '" s1=")
+    (display s1) (display '" s2=")
+    (display s2) (display '" s3=")
+    (display s3) (newline)
+        (if (= i n) (f-sum s1 s2 s3)
+          (f-inner n (+ i 1) (f-sum s1 s2 s3) s1 s2))
     )
-  (f-inner n 0 0 0)
+  (if (< n 3) n
+      (f-inner n 3 2 1 0))
   )
+
 
